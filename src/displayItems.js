2122f5b9a01d5d9fdf0720f582b38bfee488f2fd
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-16 11:47:09 -0500

    Setup enviroment variable and add getDogsData function

--- src/displayItems.js
+++ src/displayItems.js
@@ -1,8 +1,8 @@
 const mainSection = document.getElementById('main-section');
 
-function createCard(){
+function createCard() {
   const card = document.createElement('div');
-  card.classList = "main-section-card"
+  card.classList = 'main-section-card';
   card.innerHTML = `
     <div class="image-container">
 
@@ -11,7 +11,14 @@ function createCard(){
     <button class="comments-button">Comments</button>
     <button class="reservations-button">Reservations</button>
   `;
-  mainSection.appendChild(card)
+  mainSection.appendChild(card);
 }
 
 for (let i = 0; i < 9; i++)createCard();
+
+async function getDogsData() {
+  const response = await fetch('https://api.thedogapi.com/v1/breeds?page=0&limit=9', {
+    headers: { 'x-api-key': process.env.API_KEY },
+  });
+  return response.json();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -13,6 +14,7 @@ module.exports = {
       template: './src/index.html',
       title: 'API-based webapp',
     }),
+    new Dotenv(),
   ],
   output: {
     filename: 'main.js',
