79438eb1c5a0b41c5db8ac9b61f3e0d3b88353b8
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-16 13:04:32 -0500

    Display items on page

--- src/displayItems.js
+++ src/displayItems.js
@@ -1,24 +1,38 @@
 const mainSection = document.getElementById('main-section');
 
-function createCard() {
+function createCard(dog) {
   const card = document.createElement('div');
   card.classList = 'main-section-card';
   card.innerHTML = `
-    <div class="image-container">
-
+    <div class="card-image-container">
+      <img class="card-image" src="${dog.image.url}" alt="">
     </div>
-    <h3 class="card-title">Title</h3>
-    <button class="comments-button">Comments</button>
-    <button class="reservations-button">Reservations</button>
+    <h3 class="card-title">${dog.name}</h3>
+    <button id="comments-button-${dog.id}" class="comments-button">Comments</button>
+    <button id="reservations-button-${dog.id}" class="reservations-button">Reservations</button>
   `;
+
   mainSection.appendChild(card);
+
+  const commentsButton = document.getElementById(`comments-button-${dog.id}`);
+  commentsButton.addEventListener('click', () => {
+    // Comments
+  });
+
+  const reservationsButton = document.getElementById(`reservations-button-${dog.id}`);
+  reservationsButton.addEventListener('click', () => {
+    // Reservations
+  });
 }
 
-for (let i = 0; i < 9; i++)createCard();
-
-async function getDogsData() {
-  const response = await fetch('https://api.thedogapi.com/v1/breeds?page=0&limit=9', {
+const getDogsData = async () => {
+  const url = 'https://api.thedogapi.com/v1/breeds?page=0&limit=9';
+  const response = await fetch(url, {
     headers: { 'x-api-key': process.env.API_KEY },
   });
   return response.json();
-}
+};
+
+getDogsData().then((list) => {
+  list.forEach((dog) => createCard(dog));
+});
